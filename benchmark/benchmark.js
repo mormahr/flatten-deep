965c76e5ddb39a2ce16569e545170e1d395fd35f
Moritz Mahringer <me@moritzmahringer.de>
2017-02-06 21:27:59 +0100

    Setup benchmark

---
+++ benchmark/benchmark.js
@@ -0,0 +1,35 @@
+const Suite = require("benchmark").Suite;
+const flattenDeep = require("../lib/flattenDeep");
+const _ = require("lodash");
+
+const testCases = [
+    {
+        description: "multiple types, deep, short",
+        input: [10, 5, [30], ["a", ["b", ["c"], "d"], 5, true, [false, false, true]]]
+    }
+];
+
+testCases.forEach(runTestCase);
+
+function runTestCase(testCase) {
+    const suite = new Suite();
+
+    suite.add("flatten-deep", function () {
+        flattenDeep(testCases[0].input)
+    });
+
+    suite.add("_.flattenDeep", function () {
+        _.flattenDeep(testCases[0].input)
+    });
+
+    suite.on('cycle', function(event) {
+        console.log(String(event.target));
+    });
+    suite.on('complete', function() {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+    });
+
+    console.log("###################################################################");
+    console.log("Test Case:", testCase.description);
+    suite.run();
+}
