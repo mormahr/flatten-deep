834e4ab1effde2c3ca02ac1da31789874e3ca086
Moritz Mahringer <me@moritzmahringer.de>
2017-02-06 21:08:02 +0100

    Test one and two level flatten

--- src/__test__/flattenDeep.test.js
+++ src/__test__/flattenDeep.test.js
@@ -3,5 +3,13 @@ import flattenDeep from "../flattenDeep"
 describe("flattenDeep", () => {
     it("returns a non nested array as is", () => {
         expect(flattenDeep([5, "a"])).toEqual([5, "a"]);
-    })
+    });
+
+    it("flattens a one level nested array", () => {
+        expect(flattenDeep([5, "a", [3, 2]])).toEqual([5, "a", 3, 2]);
+    });
+
+    it("flattens a two level nested array", () => {
+        expect(flattenDeep([5, "a", [3, [2]]])).toEqual([5, "a", 3, 2]);
+    });
 });
